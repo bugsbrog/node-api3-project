5077760575851714fd8025bddb3fc451c98b3006
bugsbrog <bugsbrog@gmail.com>
2022-02-09 14:10:07 -0700

    validateUserId mw done and added try catch blocks to mw

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,18 +1,42 @@
+const User = require('/api/users/users-model')
+
 function logger(req, res, next) {
     console.log(`[${req.method}] ${req.url} ${new Date()}`)
     next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+    const { id } = req.params
+        try {
+            const user = await User.getById(id)
+                if (user) {
+                    req.user = user
+                    next()
+                } else {
+                    next({
+                        status: 404,
+                        message: 'user not found'
+                    })
+                }
+            } catch (err) {
+                next(err)
+            }
 }
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUser(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
 }
 
-function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+async function validatePost(req, res, next) {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports = {
