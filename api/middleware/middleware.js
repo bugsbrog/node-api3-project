f7a356ec4a3831922cb8abd566ff83b3c7e58695
bugsbrog <bugsbrog@gmail.com>
2022-02-09 14:04:53 -0700

    Finished logger mw and exported the mw's

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,6 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+    console.log(`[${req.method}] ${req.url} ${new Date()}`)
+    next()
 }
 
 function validateUserId(req, res, next) {
@@ -14,4 +15,9 @@ function validatePost(req, res, next) {
   // DO YOUR MAGIC
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+    logger,
+    validateUserId,
+    validateUser,
+    validatePost
+}
